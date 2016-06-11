82ea13f10dd8221f89024b7d6171e665fccf40d4
iamshijun <aimysaber@gmail.com>
2016-06-11 23:41:24 +0900

    optimize coordinators

--- src/main/java/com/kibou/zk/ex/ExceptionHandler.java
+++ src/main/java/com/kibou/zk/ex/ExceptionHandler.java
@@ -1,5 +1,14 @@
 package com.kibou.zk.ex;
 
+import org.apache.zookeeper.KeeperException;
+
 public abstract class ExceptionHandler {
 
+	public static void handleZkThreadEx(Exception ke){
+		if(ke instanceof InterruptedException){
+			
+		}else if(ke instanceof KeeperException){
+			
+		}
+	}
 }
